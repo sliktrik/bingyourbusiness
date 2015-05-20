ddd856812b076fdb138b104b3d08cb7cb69b4396
sliktrik <kyle@sliktrik.com.au>
2015-05-20 11:30:22 +1000

    cors

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+
 app.use('/', routes);
 app.use('/api', api);
 
