1d9b604796d80cf8f883b87711d43fcf1bf7b6c9
sliktrik <kyle@sliktrik.com.au>
2015-05-20 11:25:04 +1000

    routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -17,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', routes);
+app.use('/', routes);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,150 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+var api = "http://api.similarweb.com/Site/";
+var key = "32062136805a7a2ef5a3a055b7673bc7";
+
+
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/visits/:website', function (req, res) {
+
+    var today = new Date();
+    var endDate = today.getMonth() + "-" + today.getFullYear();
+    var startDate = (today.getMonth() - 1) + "-" + today.getFullYear();
+
+    var website = req.params.website;
+    var url = api + website + "/v1/visits?gr=daily&start=" + startDate + "&end=" + endDate + "&md=false&Format=JSON&UserKey=" + key;
+
+    request(url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var json = JSON.parse(body);
+            var total = 0;
+
+            for (var key in json.Values) {
+                total += json.Values[key].Value;
+
+                res.json({
+                    status: true,
+                    data: total,
+                    url: url
+                });
+            }
+
+        } else {
+            res.json({
+                status: false,
+                message: "no data"
+            });
+        }
+    })
+});
+
+router.get('/rank/:website', function (req, res) {
+
+    var today = new Date();
+    var endDate = today.getMonth() + "-" + today.getFullYear();
+    var startDate = (today.getMonth() - 1) + "-" + today.getFullYear();
+
+    var website = req.params.website;
+    var url = api + website + "/v1/traffic?gr=daily&start=" + startDate + "&end=" + endDate + "&md=false&Format=JSON&UserKey=" + key;
+
+    request(url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var json = JSON.parse(body);
+
+            res.json({
+                status: true,
+                data: json.GlobalRank,
+                url: url
+            });
+
+        } else {
+            res.json({
+                status: false,
+                message: "no data"
+            });
+        }
+    })
+});
+
+router.get('/orgsearch/:website', function (req, res) {
+
+    var today = new Date();
+    var endDate = today.getMonth() + "-" + today.getFullYear();
+    var startDate = (today.getMonth() - 1) + "-" + today.getFullYear();
+
+    var website = req.params.website;
+    var url = api + website + "/v1/orgsearch?gr=daily&start=" + startDate + "&end=" + endDate + "&md=false&Format=JSON&UserKey=" + key;
+
+    request(url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var json = JSON.parse(body);
+            var total = 0;
+
+            for (var key in json.Data) {
+                total += json.Data[key].Visits;
+            }
+
+            res.json({
+                status: true,
+                data: total,
+                url: url
+            });
+
+        } else {
+            res.json({
+                status: false,
+                message: "no data"
+            });
+        }
+    })
+});
+
+router.get('/paidsearch/:website', function (req, res) {
+
+    var today = new Date();
+    var endDate = today.getMonth() + "-" + today.getFullYear();
+    var startDate = (today.getMonth() - 1) + "-" + today.getFullYear();
+
+    var website = req.params.website;
+    var url = api + website + "/v1/paidsearch?gr=daily&start=" + startDate + "&end=" + endDate + "&md=false&Format=JSON&UserKey=" + key;
+
+    request(url, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            var json = JSON.parse(body);
+            var total = 0;
+
+            for (var key in json.Data) {
+                total += json.Data[key].Visits;
+            }
+
+            res.json({
+                status: true,
+                data: total,
+                url: url
+            });
+
+        } else {
+            res.json({
+                status: false,
+                message: "no data"
+            });
+        }
+    })
+});
+
+module.exports = router;
